314f88a174a8e9d30663eaf3fdb58df2d0d2c78d
unknown <arnabMondalDev>
2024-08-02 12:05:45 +0000

    exporting db file

---
+++ portfolio_manager.sql
@@ -0,0 +1,9 @@
+create database portfolio_manager;
+
+use portfolio_manager;
+
+create table holdings(asset_id int primary key, ticker varchar(10), asset_type varchar(20),qty float);
+
+create table transactions(txn_id int primary key, ticker varchar(10),txn_type varchar(5),qty float, price_rate float, txn_date datetime);
+
+create table watchlist(ticker varchar(10) primary key);
