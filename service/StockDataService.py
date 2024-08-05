f5a56fa72ed4a31950111e85b813240486a8f25b
ArnabMdev <mondalarnab15@gmail.com>
2024-08-05 23:54:44 +0530

    progress on yfinance integration

--- service/StockDataService.py
+++ service/StockDataService.py
@@ -13,10 +13,20 @@ session = CachedLimiterSession(
     backend=SQLiteCache("yfinance.cache"),
 )
 
-def getNSEStocks(count:int, period:str):
-    tickers = pd.read_html('https://ournifty.com/stock-list-in-nse-fo-futures-and-options.html#:~:text=NSE%20F%26O%20Stock%20List%3A%20%20%20%20SL,%20%201000%20%2052%20more%20rows%20')[0]
-    tickers = tickers.SYMBOL.to_list()
-    for count in range(len(tickers)):
-        tickers[count] = tickers[count] + ".NS"
-
-    price_data = yf.download(tickers[0:count] ,period=period,session=session)
+def getNSEStocks(start:int, end:int, period:str):
+    try:
+        tickers = pd.read_html('https://ournifty.com/stock-list-in-nse-fo-futures-and-options.html#:~:text=NSE%20F%26O%20Stock%20List%3A%20%20%20%20SL,%20%201000%20%2052%20more%20rows%20')[0]
+        tickers = tickers.SYMBOL.to_list()
+        for i in range(len(tickers)):
+            tickers[i] = tickers[i] + ".NS"
+        price_data = yf.Tickers(tickers[20:25] ,session=session)
+        # print(price_data.tickers)
+        for k,v in price_data.tickers.items():
+            print(k + ' ' + str(v.info['currentPrice']))
+    except Exception as err:
+        print(err)
+        return {}
+        
+    
+if __name__ == '__main__':
+    getNSEStocks(0,25,"5d")
