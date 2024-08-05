b238153a952f3d2d49873a03d71b46ca6efa0469
ArnabMdev <mondalarnab15@gmail.com>
2024-08-05 14:42:05 +0530

    Holdings CRUD implemented

--- repository/HoldingsRepository.py
+++ repository/HoldingsRepository.py
@@ -18,9 +18,93 @@ class HoldingsRepository:
             return holdings
             
         except Error as error:
-            conn.close()
+            print(error)
             cursor.close()
+            conn.close()
             return -1
+        
+    @staticmethod
+    def getHolding(asset_id):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM HOLDINGS WHERE asset_id = %s',asset_id)
+            result = cursor.fetchone()
+            cursor.close()
+            conn.close()
+            if(result):
+                holding = Holding(*result).to_dict()
+                return holding
+            else:
+                return -1
+                
+        except Error as error:
+            print(error)
+            cursor.close()
+            conn.close()
+            return -1
+    
+    @staticmethod
+    def addHolding(holding:Holding):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            insertstmnt = 'INSERT INTO HOLDINGS VALUES (%s,%s,%s,%s,%s)'
+            data = (holding.asset_id, holding.ticker, holding.asset_type, holding.qty, holding.avg_price)
+            cursor.execute(insertstmnt,data)
+            conn.commit()
+            affected_rows = cursor.rowcount
+            print(affected_rows)
+            cursor.close()
+            conn.close()
+            return affected_rows
+        except Error as error:
+            print(error)
+            cursor.close()
+            conn.close()
+            return -1
+    
+    @staticmethod
+    def updateHolding(holding:Holding):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            updatestmnt = 'UPDATE HOLDINGS SET ticker = %s, asset_type = %s, qty = %s, avg_price = %s WHERE asset_id = %s' 
+            data = (holding.ticker, holding.asset_type, holding.qty, holding.avg_price)
+            cursor.execute(updatestmnt, data)
+            conn.commit()
+            affected_rows = cursor.rowcount
+            print(affected_rows)
+            cursor.close()
+            conn.close()
+            return affected_rows
+        except Error as error:
+            print(error)
+            cursor.close()
+            conn.close()
+            return -1
+
+    @staticmethod
+    def deleteHolding(asset_id):
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            cursor.execute('DELETE FROM HOLDINGS WHERE asset_id = %s',asset_id)
+            affected_rows = cursor.rowcount()
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return affected_rows
+        except Error as error:
+            print(error)
+            cursor.close()
+            conn.close()
+            return -1
+            
+            
+            
+         
             
 if __name__ == '__main__':
     print(HoldingsRepository.getAllHoldings())
+    print(HoldingsRepository.getHolding(1))
