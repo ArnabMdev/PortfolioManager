68f69bd86a13e2485fef31dccf9331e364553b88
ArnabMdev <mondalarnab15@gmail.com>
2024-08-05 12:54:11 +0530

    Holdings Repo created

--- model/HoldingsModel.py
+++ model/HoldingsModel.py
@@ -1,9 +1,41 @@
-class HoldingsModel:
+class Holding:
     def __init__(self,asset_id, ticker="", asset_type="", qty=0,):
         self.__asset_id = asset_id
         self.__ticker = ticker
         self.__asset_type = asset_type
-        self.qty = qty
+        self.__qty = qty
+        
+    @property
+    def asset_id(self):
+        return self.__asset_id
+    
+    @property
+    def ticker(self):
+        return self.__ticker
+    
+    @property
+    def asset_type(self):
+        return self.__asset_type
+    
+    @property
+    def qty(self):
+        return self.__qty
+    
+    @asset_id.setter
+    def asset_id(self,value):
+        self.__asset_id = value
+        
+    @ticker.setter
+    def ticker(self,value):
+        self.__ticker = value
+    
+    @asset_type.setter
+    def asset_type(self,value):
+        self.__asset_type = value
+        
+    @qty.setter
+    def qty(self,value):
+        self.__qty = value
     
     def to_dict(self):
         return {
@@ -12,4 +44,7 @@ class HoldingsModel:
             "asset_type" : self.__asset_type,
             "qty" : self.__qty
             }
+        
+    def to_object(data):
+        return Holding(**data)
         
---
+++ repository/HoldingsRepository.py
@@ -0,0 +1,26 @@
+from model.HoldingsModel import Holding
+from database_connector import get_db_connection
+from mysql.connector import Error
+
+
+class HoldingsRepository:
+    
+    @staticmethod
+    def getAllHoldings():
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM HOLDINGS')
+            result = cursor.fetchall()
+            # holdings = [Holding(*item).to_dict() for item in result]
+            cursor.close()
+            conn.close()
+            return holdings
+            
+        except Error as error:
+            conn.close()
+            cursor.close()
+            return -1
+            
+if __name__ == '__main__':
+    print(HoldingsRepository.getAllHoldings())
