83cc0e5172a17cd9fb07eca4613d90ab60490b4c
unknown <arnabMondalDev>
2024-08-06 06:59:14 +0000

    Dummy commit

--- service/StockDataService.py
+++ service/StockDataService.py
@@ -22,41 +22,43 @@ session = CachedLimiterSession(
 
 
 def get_nse_stock_list(start: int, end: int):
-    tickers = pd.read_csv('../data/StocksTraded.csv',index_col=1)['Symbol']
-    print(tickers)
+    tickers = pd.read_csv('../data/StocksTraded.csv',index_col=1)['Symbol '].tolist()
     ticker_list = []
-    # for i in range(start, end, 1):
-        # ticker_list = tickers[i] + ".NS"
+    for i in range(start, end, 1):
+        ticker_list.append(tickers[i] + ".NS")
     return ticker_list
 
 
 def get_nse_stock_data(start: int, end: int):
     try:
         tickers = get_nse_stock_list(start, end)
-        raw_price_data = yf.Tickers(tickers[start:end], session=session)
+        print(tickers)
+        raw_price_data = yf.Tickers(tickers, session=session)
+        print(raw_price_data.tickers)
         price_datas = []
-        for k, v in raw_price_data.tickers.items():
-            if k is None or v is None:
-                continue
-            price_data = PriceData(
-                str(k),
-                v.info['open'],
-                v.info['close'],
-                {
-                    'dayHigh': v.info['dayHigh'],
-                    'regularMarketDayHigh': v.info['regularMarketDayHigh'],
-                    'fiftyTwoWeekHigh': v.info['fiftyTwoWeekHigh']
-                },
-                {
-                    'dayLow': v.info['dayLow'],
-                    'regularMarketDayLow': v.info['regularMarketDayLow'],
-                    'fiftyTwoWeekLow': v.info['fiftyTwoWeekLow']
-                },
-                v.info['volume'],
-                v.info['current_price'],
-                datetime.datetime.now()
-            )
-            price_datas.append(price_data)
+        # for k, v in raw_price_data.tickers.items():
+        #     if k is None or v is None:
+        #         continue
+        #     print(v.info)
+            # price_data = PriceData(
+            #     str(k),
+            #     v.info['open'],
+            #     v.info['close'],
+            #     {
+            #         'dayHigh': v.info['dayHigh'],
+            #         'regularMarketDayHigh': v.info['regularMarketDayHigh'],
+            #         'fiftyTwoWeekHigh': v.info['fiftyTwoWeekHigh']
+            #     },
+            #     {
+            #         'dayLow': v.info['dayLow'],
+            #         'regularMarketDayLow': v.info['regularMarketDayLow'],
+            #         'fiftyTwoWeekLow': v.info['fiftyTwoWeekLow']
+            #     },
+            #     v.info['volume'],
+            #     v.info['current_price'],
+            #     datetime.datetime.now()
+            # )
+            # price_datas.append(price_data)
         return price_datas
     except Exception as err:
         print(err)
@@ -64,4 +66,4 @@ def get_nse_stock_data(start: int, end: int):
 
 
 if __name__ == '__main__':
-    get_nse_stock_list(0, 25)
+    print(get_nse_stock_data(0, 25))
