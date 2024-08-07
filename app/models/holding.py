561b64f8c20219bb7c99c8426ff0ce6508f70451
ArnabMdev <mondalarnab15@gmail.com>
2024-08-07 14:20:37 +0530

    Sell Transaction implemented

--- app/models/holding.py
+++ app/models/holding.py
@@ -1,17 +1,15 @@
 from app import db
 
+
 class Holding(db.Model):
     __tablename__ = 'holdings'
-
-    asset_id = db.Column(db.String(255), primary_key=True)
+    ticker = db.Column(db.String(20), nullable=False, primary_key=True)
     user_id = db.Column(db.String(255), nullable=False, index=True, default="user1")
-    ticker = db.Column(db.String(20), nullable=False)
     asset_type = db.Column(db.String(50), nullable=False)
     qty = db.Column(db.Float, nullable=False, default=0)
     avg_price = db.Column(db.Float, nullable=False, default=0)
 
-    def __init__(self, asset_id, user_id="user1", ticker="", asset_type="", qty=0, avg_price=0):
-        self.asset_id = asset_id
+    def __init__(self, user_id="user1", ticker="", asset_type="", qty=0, avg_price=0):
         self.user_id = user_id
         self.ticker = ticker
         self.asset_type = asset_type
@@ -20,9 +18,8 @@ class Holding(db.Model):
 
     def to_dict(self):
         return {
-            "asset_id": self.asset_id,
-            "user_id": self.user_id,
             "ticker": self.ticker,
+            "user_id": self.user_id,
             "asset_type": self.asset_type,
             "qty": self.qty,
             "avg_price": self.avg_price
@@ -31,3 +28,5 @@ class Holding(db.Model):
     @staticmethod
     def from_dict(data):
         return Holding(**data)
+
+
--- app/services/holding_service.py
+++ app/services/holding_service.py
@@ -2,6 +2,7 @@ from app import db
 from app.models.holding import Holding
 from sqlalchemy.exc import SQLAlchemyError
 
+
 class HoldingService:
     @staticmethod
     def get_all_holdings():
@@ -10,14 +11,6 @@ class HoldingService:
         except SQLAlchemyError as e:
             db.session.rollback()
             raise Exception(f"Failed to retrieve holdings: {str(e)}")
-
-    @staticmethod
-    def get_holding_by_asset_id(asset_id):
-        try:
-            return Holding.query.get(asset_id)
-        except SQLAlchemyError as e:
-            db.session.rollback()
-            raise Exception(f"Failed to retrieve holding: {str(e)}")
 
     @staticmethod
     def get_holdings_by_user_id(user_id):
@@ -39,16 +32,16 @@ class HoldingService:
             raise Exception(f"Failed to create holding: {str(e)}")
 
     @staticmethod
-    def update_holding(asset_id, data):
+    def update_holding(ticker, data):
         try:
-            holding = Holding.query.get(asset_id)
+            holding = Holding.query.get(ticker)
             if not holding:
                 raise Exception("Holding not found")
-            
+
             for key, value in data.items():
                 if hasattr(holding, key):
                     setattr(holding, key, value)
-            
+
             db.session.commit()
             return holding
         except SQLAlchemyError as e:
@@ -56,12 +49,12 @@ class HoldingService:
             raise Exception(f"Failed to update holding: {str(e)}")
 
     @staticmethod
-    def delete_holding(asset_id):
+    def delete_holding(ticker):
         try:
-            holding = Holding.query.get(asset_id)
+            holding = Holding.query.get(ticker)
             if not holding:
                 raise Exception("Holding not found")
-            
+
             db.session.delete(holding)
             db.session.commit()
             return True
@@ -75,7 +68,7 @@ class HoldingService:
             return Holding.query.filter_by(ticker=ticker).all()
         except SQLAlchemyError as e:
             db.session.rollback()
-            raise Exception(f"Failed to retrieve holdings by ticker: {str(e)}")
+            return None
 
     @staticmethod
     def get_holdings_by_asset_type(asset_type):
@@ -86,18 +79,41 @@ class HoldingService:
             raise Exception(f"Failed to retrieve holdings by asset type: {str(e)}")
 
     @staticmethod
-    def update_holding_quantity(asset_id, quantity_change):
+    def update_holding_quantity(ticker:str, quantity_change:float):
         try:
-            holding = Holding.query.get(asset_id)
+            holding = Holding.query.get(ticker)
             if not holding:
+                print(ticker)
                 raise Exception("Holding not found")
-            
+
+
             holding.qty += quantity_change
+
+            if holding.qty == 0:
+                db.session.delete(holding)
+                db.session.commit()
+                return
             if holding.qty < 0:
                 raise Exception("Holding quantity cannot be negative")
-            
+
             db.session.commit()
             return holding
         except SQLAlchemyError as e:
             db.session.rollback()
             raise Exception(f"Failed to update holding quantity: {str(e)}")
+
+    @staticmethod
+    def update_avg_price(ticker, qty, buy_price):
+        try:
+            holding = Holding.query.get(ticker)
+            if not holding:
+                raise Exception("Holding not found")
+
+            holding.avg_price = (buy_price * qty + holding.avg_price * holding.qty) / (qty + holding.qty)
+            holding.qty += qty
+            db.session.commit()
+            return holding
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            raise Exception(f"Failed to update holding quantity: {str(e)}")
+
