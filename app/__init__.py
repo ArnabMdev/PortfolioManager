691c1cf5d85d60fa803a66b53a2ade5f140284c2
Aditi Agarwal <aditi2k1@gmail.com>
2024-08-08 14:07:54 +0530

    Swagger Integration

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .config import Config
+from flask_swagger_ui import get_swaggerui_blueprint
 
 db = SQLAlchemy()
 migrate = Migrate()
 
+#swagger configs
+SWAGGER_URL= '/api/docs'
+API_URL= '/static/swagger.json'
+SWAGGER_BLUEPRINT= get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'StashDash APIs'
+    }
+)
 
 def create_app():
     app = Flask(__name__)
@@ -14,12 +25,12 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from .routes import (user_routes, transaction_routes, holding_routes,
-                         price_data_route, transaction_routes, price_history_route)
+    from .routes import (user_routes, transaction_routes, holding_routes,price_data_route, transaction_routes, price_history_route)
     app.register_blueprint(user_routes.bp)
     app.register_blueprint(holding_routes.bp)
     app.register_blueprint(price_data_route.bp)
     app.register_blueprint(price_history_route.bp)
     app.register_blueprint(transaction_routes.bp)
+    app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix= SWAGGER_URL)
 
     return app
